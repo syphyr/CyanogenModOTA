438b0c9d4861d5b54ac31b777693b3018d0018c9
syphyr <syphyr@gmail.com>
2020-09-13 22:14:14 +0200

    Fix isValid for legacy ROMs

--- src/Helpers/Build.php
+++ src/Helpers/Build.php
@@ -106,8 +106,6 @@
          * @return boolean True if valid, False if not.
          */
     	public function isValid($params){
-            if ( $params === NULL ) return true;  // Assume valid if no parameters
-
             $ret = false;
 
             if ( $params['device'] == $this->model ) {
